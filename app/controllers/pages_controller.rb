7abcaf5508d2f5ae31458f3544e57e92dbb764ed
Arthur Littmann <arthur.littmann@hey.com>
2020-08-18 12:47:32 +0100

    seed and home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # @weeks = WorkDay.where('date > ?', Date.today) # won't work because we want to show the current week
+    @weeks = WorkDay.all.group_by { |wd| wd.date.cweek }
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+today = Date.today
+
+last_day = Date.new(2020,12,31)
+
+today.upto(last_day) do |date|
+  unless date.strftime("%A").downcase == 'sunday'
+    WorkDay.create!(date: date)
+    puts "Added #{date.strftime('%A, %b %d')}"
+  end
+end
